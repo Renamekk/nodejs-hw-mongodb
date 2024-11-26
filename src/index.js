876ed96a20e16f8907d5af3f8e00603000a26eae
Renamekk <black80512@gmail.com>
2024-11-26 19:53:06 +0100

    nodejs-hw-mongodb

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import dotenv from 'dotenv';
 import { setupServer } from './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 
+
+
 dotenv.config();
 
 const startApp = async () => {
   try {
     await initMongoConnection();
-
     setupServer();
   } catch (error) {
     console.error('Error starting the application:', error.message);
