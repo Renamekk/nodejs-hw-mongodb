80ebabffaa440fa046818a4d3906ec4d229ff19b
Renamekk <black80512@gmail.com>
2024-11-27 13:51:47 +0100

    nodejs-hw-mongodb

--- src/server.js
+++ src/server.js
@@ -3,18 +3,25 @@ import cors from 'cors';
 import pino from 'pino-http';
 import contactsRouter from './routes/contacts.js'; 
 import mongoose from 'mongoose'; 
+import dotenv from 'dotenv'; 
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    const mongoURI = process.env.MONGO_URI;
+    if (!mongoURI) {
+      throw new Error("MONGO_URI is not defined in the .env file");
+    }
+    await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB", error);
-    process.exit(1);  
+    process.exit(1); 
   }
 };
 
-export const setupServer = () => {
+const setupServer = () => {
   const app = express();
 
   app.use(cors());
